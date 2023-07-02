6cf9838c9ed0d90cde7e1a16bc1c448d7b509cf3
HazemShehawy <Hazemk537@gmail.com>
2023-07-02 23:45:42 +0300

    ignore

--- src/App.js
+++ src/App.js
@@ -1,70 +1,63 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import * as ReactDOM from 'react-dom'
-import * as React from 'react'
-import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
 import { action as destroyAction } from "./routes/destroy";
 
 import ErrorPage from "./error-page";
-import ErrorPage2 from "./error-2";
-
 /* existing code */
 import Contact, {
-  loader as contactLoader,  
+  loader as contactLoader,
   action as contactAction,
 } from "./routes/contact";
 
-import EditContact, {
-  action as editAction,
-} from "./routes/edit";
+import EditContact, { action as editAction } from "./routes/edit";
 import Index from "./routes";
 
-const router = createBrowserRouter([{
-
-  path: "/",
-  element: <Root />,
-  errorElement: <ErrorPage />
-  , loader: rootLoader
-  , action: rootAction
-  , children:[{
-    errorElement: <ErrorPage />
-    ,children: [     
-       { index: true, element: <Index/> },
-    {
-      path: "contacts/:contactId",
-      element: <Contact />,
-      loader: contactLoader,
-      action: contactAction,
-      
-  
-  
-    }, {
-      path: "contacts/:contactId/edit",
-      element: <EditContact />,
-      loader: contactLoader,
-      action: editAction,
-  
-    },{
-      path: "contacts/:contactId/destroy",
-      action: destroyAction,
-      errorElement: <div>Oops! There was an error.</div>,
-  
-    }]}
-
-
-]
-},])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
   ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-
       <RouterProvider router={router} />
-
     </React.StrictMode>
-
-  )
-
+  );
 }
 
 export default App;
-
