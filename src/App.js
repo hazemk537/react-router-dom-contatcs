f141c151a28f14902d9b6b7b336012af3e4299e2
HazemShehawy <Hazemk537@gmail.com>
2023-07-02 19:19:36 +0300

    app with repeatition error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import * as ReactDOM from 'react-dom'
+import * as React from 'react'
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import { action as destroyAction } from "./routes/destroy";
+
+import ErrorPage from "./error-page";
+import ErrorPage2 from "./error-2";
+
+/* existing code */
+import Contact, {
+  loader as contactLoader,  
+  action as contactAction,
+} from "./routes/contact";
+
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
+import Index from "./routes";
+
+const router = createBrowserRouter([{
+
+  path: "/",
+  element: <Root />,
+  errorElement: <ErrorPage />
+  , loader: rootLoader
+  , action: rootAction
+  , children:[{
+    errorElement: <ErrorPage />
+    ,children: [     
+       { index: true, element: <Index/> },
+    {
+      path: "contacts/:contactId",
+      element: <Contact />,
+      loader: contactLoader,
+      action: contactAction,
+      
+  
+  
+    }, {
+      path: "contacts/:contactId/edit",
+      element: <EditContact />,
+      loader: contactLoader,
+      action: editAction,
+  
+    },{
+      path: "contacts/:contactId/destroy",
+      action: destroyAction,
+      errorElement: <div>Oops! There was an error.</div>,
+  
+    }]}
+
+
+]
+},])
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+
+      <RouterProvider router={router} />
+
+    </React.StrictMode>
+
+  )
+
 }
 
 export default App;
+
